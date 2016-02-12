f0cf7bbcc3a4ef3baa4cfd0d327c70190388d904
Christian James Balan <balanc@colorado.edu>
2016-02-12 02:00:36 -0700

    Updated project styling and javascript. Added image for background. Project is near 90% complete.

--- js/github-js.js
+++ js/github-js.js
@@ -1,20 +1,23 @@
 $(document).ready(function(){
 
-	//variables
+	/* ========== Begin Variables ==========*/
 
-	var user = $('#field');
+
+	var user = $('#field'); //get text field id
 	var value;
 	var check = true;
 	var text;
-	// var val = user.val().toLowerCase(); //get user input
 
-	//functions
+	/* ========== End Variables ==========*/
+
+	/* ========== Begin Functions ==========*/
 
 	//clear text field
 	function clearUser(){
 		user.val("");
 	}
 
+	//toggle letter contents
 	function showText(){
 		$("p.hide").animate({
 			width: 'toggle',
@@ -22,40 +25,50 @@ $(document).ready(function(){
 		});
 	}
 
+	//unimplemented function gets contents of all paragraph tags for letter
 	function letterText(){
 		$("p.hide").each(function(){
 			text += $(this).html() + ' ';
 		});
 	}
 
+	/* ========== End Functions ==========*/
+
+	/* ========== Begin Page Interaction ==========*/
+
+
 	//get value of field after enter key
 	$("#field").keyup(function(e){
 	    if (e.keyCode == 13) {
-	        // console.log($(user).val().toLowerCase());
-	        value = $(user).val().toLowerCase();
 
-	        if(value == 'git log' && check == true)
+	        // console.log($(user).val().toLowerCase());
+	        value = $(user).val().toLowerCase(); //set value equal to user input after enter keyup
+
+	        if(value == 'git love' && check == true)
 	        {
-	        	showText();
+	        	showText(); //toggle letter contents
+	        	$('.add').remove(); //remove appended text if existing
 	        	check = false
 
 	        } else if(value == 'clear' && check == false) {
 
-	        	showText();
-	        	$('.add').remove();
+	        	showText(); //toggle letter contents
+	        	$('.add').remove(); //remove appended text if existing
 	        	check = true
+
+	        } else if(value == 'clear') {
+
+	        	$('.add').remove(); //remove appended text if existing
 
 	        } else {
 
-				$(".terminal").append('<span class="add">'+"Just follow directions"+'</span>');
+				$(".terminal").append('<span class="add"><p>-bash: ' + value + ': command not found</p></span>'); //append text if user inputs incorrectly
 	        }
 
-	        clearUser();
+	        clearUser(); //clear text field on enter keyup
 	    }
 	});
 
-	letterText();
-
-	// writeLog();
+	/* ========== End Page Interaction ==========*/
 
 });
